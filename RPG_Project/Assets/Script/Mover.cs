91b64298b43addcb1a91be06f821193bddf263b3
İrem Asena Kaplan <81148564+iremasenakaplan@users.noreply.github.com>
2024-01-07 22:28:01 +0300

    Navmesh mec.

---
+++ RPG_Project/Assets/Script/Mover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    Ray lastRay;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction*100, Color.red);
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
