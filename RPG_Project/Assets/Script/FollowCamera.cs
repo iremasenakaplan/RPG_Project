713de771c1f0c95e5ceaf54506739cc58e876683
İrem Asena Kaplan <81148564+iremasenakaplan@users.noreply.github.com>
2024-01-13 22:14:32 +0300

    .

--- RPG_Project/Assets/Script/FollowCamera.cs
+++ RPG_Project/Assets/Script/FollowCamera.cs
@@ -6,7 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
     [SerializeField] Transform player;
 
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.position;
     }
