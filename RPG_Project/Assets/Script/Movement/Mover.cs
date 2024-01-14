dbd4b9f34d7d498b485d84a3f1ba3069355e93f1
İrem Asena Kaplan <81148564+iremasenakaplan@users.noreply.github.com>
2024-01-14 22:43:08 +0300

    player controller

---
+++ RPG_Project/Assets/Script/Movement/Mover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void MoveTo(Vector3 hit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit;
+        }
+
+        public void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+    }
+}
+
+
