79c8110fa6faaab91669b056ccb81d19bae187ec
İrem Asena Kaplan <81148564+iremasenakaplan@users.noreply.github.com>
2024-01-15 22:51:15 +0300

    Attack Mechanics

--- RPG_Project/Assets/Script/Controller/PlayerController.cs
+++ RPG_Project/Assets/Script/Controller/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Controller
 {
@@ -9,22 +10,44 @@ namespace RPG.Controller
     {
         void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
             }
+        }
+
+        private void InteractWithCombat()
+        {
+            
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach(RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+            }
 
         }
+
         public void MoveToCursor()
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        { 
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit == true)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
