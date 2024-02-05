74117bb3375dd8667cc791ba02655150a0f7eb90
İrem Asena Kaplan <81148564+iremasenakaplan@users.noreply.github.com>
2024-02-05 19:50:09 +0300

    Patrol mechanism

--- RPG_Project/Assets/Script/Controller/PatrolPath.cs
+++ RPG_Project/Assets/Script/Controller/PatrolPath.cs
@@ -11,10 +11,26 @@ namespace RPG.Controller
         {
             for(int i = 0; i< transform.childCount; i++)
             {
-                Gizmos.DrawSphere(transform.GetChild(i).position, wayPointRadius);
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWayPointPosition(i), wayPointRadius);
+                Gizmos.DrawLine(GetWayPointPosition(i), GetWayPointPosition(j));
             }
-           
-           
+
+
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if(i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWayPointPosition(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
